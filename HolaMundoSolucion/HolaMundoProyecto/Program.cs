517291cf1e78ea3fec90a4c3f814523968e0e800
Eimar Ramos <eimar.ramossacari@gmail.com>
2025-07-16 11:36:21 +0200

    Primer Hola Mundo

---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Hola, Mundo!");
+
+        /*
+         *     //
+             _oo\
+            (__/ \  _  _
+               \  \/ \/ \
+               (         )\
+                \_______/  \
+                 [[] [[]
+                 [[] [[]
+         */
+
+        Console.WriteLine(@"    //");
+        Console.WriteLine(@"  _oo \");
+        Console.WriteLine(@" (__/  \  _  _");
+        Console.WriteLine(@"     \  \/ \/ \");
+        Console.WriteLine(@"     (         )\");
+        Console.WriteLine(@"      \_______/  \");
+        Console.WriteLine(@"       [[] [[]");
+        Console.WriteLine(@"       [[] [[]");
+
+    }
+}
